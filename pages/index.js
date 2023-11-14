1c62614dafb877ec775cff8e46414769f09a4dcd
syennings <syhennings@live.com>
2023-11-14 16:42:48 +0100

    set up swr fetch

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(URL);
+  console.log(data);
   return (
     <div>
       <h1>Hello from Next.js</h1>
