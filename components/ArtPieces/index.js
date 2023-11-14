9a4a0fe9268c042a3e5d4153cef401fd2422e4da
syennings <syhennings@live.com>
2023-11-14 17:13:45 +0100

    art pieces first entries

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,1 +1,15 @@
-import useSWR from "swr";
+export function ArtPieces({ pieces }) {
+  return (
+    <>
+      <ul>
+        {pieces.map((piece) => (
+          <li key={piece.slug}>
+            <h3>{piece.name}</h3>
+            <p>Artist: {piece.artist}</p>
+            {/* <img src={piece.imageSource} alt={piece.name} /> */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import useSWR from "swr";
+import { ArtPieces } from "@/components/ArtPieces";
 
 const URL = "https://example-apis.vercel.app/api/art";
 
 export default function HomePage() {
   const { data, error, isLoading } = useSWR(URL);
+
   console.log(data);
+
+  if (error) return <div> failed to load</div>;
+  if (isLoading) return <div> is loading ....</div>;
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <ArtPieces pieces={data}> Testing the art pieces </ArtPieces>
     </div>
   );
 }
